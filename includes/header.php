6dab5bccf07fb6dae111980e8fa33e32e120902f
Luiz Neto <luizinholuiza@gmail.com>
2024-08-19 23:48:51 -0300

    inicio

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">Ana Julia Motta Beauty</a>
+       
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a href="index.php" class="nav-link">Inicio</a></li>
+            <li class="nav-item"><a href="services.php" class="nav-link">Serviços</a></li>
+            
+            <li class="nav-item"><a href="about.php" class="nav-link">Sobre</a></li>
+           
+            <li class="nav-item"><a href="contact.php" class="nav-link">Contato</a></li>
+            
+            <li class="nav-item"><a href="admin/index.php" class="nav-link">Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
